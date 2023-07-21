3f7ae71e04f53149f78d9e080305e65bed3b1d75
Aman bhai <rohanrikwar9555@gmail.com>
2023-07-22 02:58:20 +0530

    new5-q

--- Q-1/script.js
+++ Q-1/script.js
@@ -222,3 +222,239 @@ function devide(){
     let ans= input_1.value / input_2.value
     anss.innerHTML=`the result is  : ${ans}`
 }
+
+
+/**
+ * 11 Question 
+ * 
+ 
+
+//////////////--------------------****************************-----------------///////////////////////
+*/
+
+
+function cel(calsuse){
+   let cell=calsuse;
+   let celtof=cell * 9/5 +32;
+   let mess= `${cell}°C is  ${celtof}°F`;
+   console.log(mess)
+}
+cel(45);
+function fToC(fahrenheit) 
+{
+  const fTemp = fahrenheit;
+  const fToCel = (fTemp - 32) * 5 / 9;
+  const message = `${fTemp}\xB0F is ${fToCel}\xB0C.`;
+    console.log(message);
+} 
+fToC(45)
+
+
+/**
+ * 12 Question 
+ * 
+ 
+
+//////////////--------------------****************************-----------------///////////////////////
+*/
+
+
+
+let curuntwurl=document.URL;
+console.log(curuntwurl)
+let pin=document.querySelector(".loadingurl");
+pin.innerHTML=curuntwurl
+
+/**
+ * 13 Question 
+ * 
+ 
+
+//////////////--------------------****************************-----------------///////////////////////
+*/
+
+
+let username='abcd';
+let nu=12;
+this[username]=nu;
+console.log(this[username])
+//console.log(this.username)
+console.log(window)
+/**
+ * 14 Question 
+ * 
+ 
+
+//////////////--------------------****************************-----------------///////////////////////
+*/
+
+let filename="ravanfaile.php";
+let fname=filename.split(".").pop()
+console.log(fname)
+let filenMAME="havabazi.js";
+let fnamE=filenMAME.split(".").pop()
+console.log(fnamE)
+
+
+
+
+/**
+ * 15 Question 
+ * 
+ 
+
+//////////////--------------------****************************-----------------///////////////////////
+*/
+
+
+
+function addin(x,y){
+    if(x==y){
+        return 3*(x+y);
+
+    }else{
+        return x+y
+    }
+}
+console.log(addin(4,4))
+
+
+
+
+
+
+/**
+ * 16 Question 
+ * 
+ 
+
+//////////////--------------------****************************-----------------///////////////////////
+*/
+
+function checknum(first,secend){
+    if(first==50 || secend==50 || secend+first==50){
+        return true;
+    }else{
+        return first+secend;
+
+    }
+}
+
+console.log(checknum(10,10))
+
+
+/**
+ * 17 Question 
+ * 
+ 
+
+//////////////--------------------****************************-----------------///////////////////////
+*/
+
+
+function defrence(n){
+    if(n <=19){
+        return 19-n
+    }else{
+        return (n-19)*3
+    }
+}
+console.log(defrence(5))
+
+
+
+
+
+/**
+ * 18 Question 
+ * 
+ 
+
+//////////////--------------------****************************-----------------///////////////////////
+*/
+
+
+
+function checkk(n){
+    return (Math.abs((100-n)<=20)||Math.abs((400-n)<=20))
+}
+console.log(checkk(9))
+
+
+
+/**
+ * 19 Question 
+ * 
+ 
+
+//////////////--------------------****************************-----------------///////////////////////
+*/
+
+
+
+function checkNagitave(x,y){
+ if((x>0 && y<0)||(x<0&&y> 0)){
+    return true;
+ }else{
+    return false
+ }
+}
+
+console.log(checkNagitave(2,2))
+console.log(checkNagitave(-2,2))
+console.log(checkNagitave(2,-2))
+console.log(checkNagitave(-2,-2))
+
+
+
+/**
+ * 20 Question 
+ * 
+ 
+
+//////////////--------------------****************************-----------------///////////////////////
+*/
+
+
+
+
+function checks(x){
+    if(x===null||x===undefined||x.substring(0,2)==="py"){
+        return x
+    }else{
+        return "py"+x
+    }
+}
+
+console.log(checks("pyotone"))
+
+
+/**
+ * 21 Question 
+ * 
+ 
+
+//////////////--------------------****************************-----------------///////////////////////
+*/
+
+
+
+
+function rev(x){
+    let a= Array.from(x)
+   let b= a.reverse()
+   let c=b.join("")
+   return c.toString()
+}
+console.log(rev("hello i ama aman"))
+/**OR */
+function st(xx){
+    if(xx===null||xx===undefined||xx.length<=1){
+        return xx
+    }else{
+        let carr= xx.substring(1,xx.length-1);
+        let re=(xx.charAt(xx.length-1)+carr+xx.charAt(0))
+        console.log(re)
+    }
+}
+st("animal")
